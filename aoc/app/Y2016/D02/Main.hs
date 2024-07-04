a9222f07a69d768936611e9e6a41623c2b54b908
Matt Wittmann <mcwitt@gmail.com>
2024-07-04 09:51:48 -0700

    2016 day 2: add Show instances, clean

--- aoc/app/Y2016/D02/Main.hs
+++ aoc/app/Y2016/D02/Main.hs
@@ -15,7 +15,8 @@ solution = AoC.Solution parse solve1 solve2
 parse :: String -> [[Dir]]
 parse = (map . map) (read . (: [])) . lines
 
-data Three = One | Two | Three deriving (Enum)
+data Three = One | Two | Three
+  deriving (Enum)
 
 inc :: Three -> Three
 inc One = Two
@@ -29,76 +30,61 @@ dec Three = Two
 
 type Key = (Three, Three)
 
-update :: Key -> Dir -> Key
-update (r, c) = go
+instance {-# OVERLAPPING #-} Show [Key] where
+  show = map (\(r, c) -> let n = 1 + fromEnum c + 3 * fromEnum r in intToDigit n)
+
+upd :: Key -> Dir -> Key
+upd (r, c) = go
   where
     go U = (dec r, c)
     go D = (inc r, c)
     go L = (r, dec c)
     go R = (r, inc c)
 
-keyChar :: Key -> Char
-keyChar (r, c) = let n = 1 + fromEnum c + 3 * fromEnum r in intToDigit n
+solve :: (Show [key]) => key -> (key -> Dir -> key) -> [[Dir]] -> String
+solve start update = show . tail . scanl' (foldl' update) start
 
-solve :: key -> (key -> Dir -> key) -> (key -> Char) -> [[Dir]] -> String
-solve start update keyChar = tail . map keyChar . scanl' (foldl' update) start
+solve1 = solve (Two, Two) upd
 
-solve1 = solve (Two, Two) update keyChar
-
-data Key2
-  = K1
-  | K2
-  | K3
-  | K4
-  | K5
-  | K6
-  | K7
-  | K8
-  | K9
-  | KA
-  | KB
-  | KC
-  | KD
+data Key2 = K1 | K2 | K3 | K4 | K5 | K6 | K7 | K8 | K9 | KA | KB | KC | KD
   deriving (Enum)
 
-update2 :: Key2 -> Dir -> Key2
-update2 K1 D = K3
-update2 K2 D = K6
-update2 K2 R = K3
-update2 K3 U = K1
-update2 K3 D = K7
-update2 K3 L = K2
-update2 K3 R = K4
-update2 K4 D = K8
-update2 K4 L = K3
-update2 K5 R = K6
-update2 K6 U = K2
-update2 K6 D = KA
-update2 K6 L = K5
-update2 K6 R = K7
-update2 K7 U = K3
-update2 K7 D = KB
-update2 K7 L = K6
-update2 K7 R = K8
-update2 K8 U = K4
-update2 K8 D = KC
-update2 K8 L = K7
-update2 K8 R = K9
-update2 K9 L = K8
-update2 KA U = K6
-update2 KA R = KB
-update2 KB U = K7
-update2 KB D = KD
-update2 KB L = KA
-update2 KB R = KC
-update2 KC U = K8
-update2 KC L = KB
-update2 KD U = KB
-update2 k _ = k
+instance {-# OVERLAPPING #-} Show [Key2] where
+  show = map (\k -> let n = fromEnum k + 1 in if n < 10 then intToDigit n else toEnum (fromEnum 'A' + n - 10))
 
-keyChar2 :: Key2 -> Char
-keyChar2 k =
-  let n = fromEnum k + 1
-   in if n < 10 then intToDigit n else toEnum (fromEnum 'A' + n - 10)
+upd2 :: Key2 -> Dir -> Key2
+upd2 K1 D = K3
+upd2 K2 D = K6
+upd2 K2 R = K3
+upd2 K3 U = K1
+upd2 K3 D = K7
+upd2 K3 L = K2
+upd2 K3 R = K4
+upd2 K4 D = K8
+upd2 K4 L = K3
+upd2 K5 R = K6
+upd2 K6 U = K2
+upd2 K6 D = KA
+upd2 K6 L = K5
+upd2 K6 R = K7
+upd2 K7 U = K3
+upd2 K7 D = KB
+upd2 K7 L = K6
+upd2 K7 R = K8
+upd2 K8 U = K4
+upd2 K8 D = KC
+upd2 K8 L = K7
+upd2 K8 R = K9
+upd2 K9 L = K8
+upd2 KA U = K6
+upd2 KA R = KB
+upd2 KB U = K7
+upd2 KB D = KD
+upd2 KB L = KA
+upd2 KB R = KC
+upd2 KC U = K8
+upd2 KC L = KB
+upd2 KD U = KB
+upd2 k _ = k
 
-solve2 = solve K5 update2 keyChar2
+solve2 = solve K5 upd2
